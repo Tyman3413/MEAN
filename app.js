f88457c46ca19784fecb8f38ade1aeec49e80507
Tyman3413 <tyman3413@gmail.com>
2022-06-24 22:19:57 +0300

    point_2.2

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
-var express = require("express"); /* Подключаем модуль Express */
-var app = express(); /* Инициализация приложения */
+/* // Подключаем модули */
+var express = require("express");
+var bodyParser = require("body-parser");
+
+/* Инициализация приложения */
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 /* Обработка рутового маршрута браузера */
 app.get("/", function (req, res) {
-    res.send("Серверная информация для браузера");
+    var form =
+        "<!doctype html>" +
+        '<html lang="ru">' +
+        "<head>" +
+        '<meta charset="UTF-8">' +
+        "<title>Форма</title>" +
+        "</head>" +
+        "<body>" +
+        "<h1>Форма для отправки данных на сервер</h1>" +
+        '<form action="/submit_result" method="post">' +
+        '<textarea name="text"></textarea><br/><br/>' +
+        '<input type="submit" value="Отправить данные на сервер"/>' +
+        "</form>" +
+        "</body>" +
+        "</html>";
+    res.send(form);
 });
 
-/* Запускаем Node.js сервер на порту 3000 */
-app.listen("3000");
+/* Извлечения данных формы */
+app.post("/submit_result", function (req, res) {
+    console.log(req.body);
+    var post_text = req.body.text
+        ? "Вы отправили на сервер текст: " + req.body.text
+        : "Вы отправили на сервер пустую строку";
+    res.send(post_text);
+});
+
+/* Добавим Callback функцию */
+app.listen("3000", function () {
+    console.log("Сервер работает и слушает порт: 3000");
+});
