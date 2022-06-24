ba3d1489703f47e849b3d36084e3974a9ef62235
Tyman3413 <tyman3413@gmail.com>
2022-06-24 22:59:29 +0300

    point_4.2

--- all/routes/index.js
+++ all/routes/index.js
@@ -1,9 +1,34 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+    res.render("index", { title: "Express" });
+});
+
+/* Страница Молнии МакКуина */
+router.get("/mcqueen", function (req, res, next) {
+    res.send("<h1>Страница Молнии МакКуина</h1>");
+});
+
+/* Страница Дока Хадсона */
+router.get("/dochudson", function (req, res, next) {
+    res.send("<h1>Страница Дока Хадсона</h1>");
+});
+
+/* Страница Ченга Кинга */
+router.get("/king", function (req, res, next) {
+    res.send("<h1>Страница Ченга Кинга</h1>");
+});
+
+/* Страница Чико Хикса */
+router.get("/chickhicks", function (req, res, next) {
+    res.send("<h1>Страница Чико Хикса</h1>");
+});
+
+/* Страница Салли */
+router.get("/sally", function (req, res, next) {
+    res.send("<h1>Страница Салли</h1>");
 });
 
 module.exports = router;
