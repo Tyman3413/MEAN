6baef3fe27edb5aa73c20f574bd2ad9747d96848
Tyman3413 <tyman3413@gmail.com>
2022-06-24 23:13:02 +0300

    point_4.3

--- all/routes/index.js
+++ all/routes/index.js
@@ -8,27 +8,47 @@ router.get("/", function (req, res, next) {
 
 /* Страница Молнии МакКуина */
 router.get("/mcqueen", function (req, res, next) {
-    res.send("<h1>Страница Молнии МакКуина</h1>");
+    res.render("hero", {
+        title: "Молния МакКуин",
+        picture: "images/mcqueen",
+        desc: "Красный Roockie Race Car 2006",
+    });
 });
 
 /* Страница Дока Хадсона */
 router.get("/dochudson", function (req, res, next) {
-    res.send("<h1>Страница Дока Хадсона</h1>");
+    res.render("hero", {
+        title: "Док Хадсон",
+        picture: "images/dochudson",
+        desc: "Темно-синий Hudson Hornet 1951",
+    });
 });
 
 /* Страница Ченга Кинга */
 router.get("/king", function (req, res, next) {
-    res.send("<h1>Страница Ченга Кинга</h1>");
+    res.render("hero", {
+        title: "Ченг Кинг",
+        picture: "images/king",
+        desc: "Голубой Plymouth Superbird 1970",
+    });
 });
 
 /* Страница Чико Хикса */
 router.get("/chickhicks", function (req, res, next) {
-    res.send("<h1>Страница Чико Хикса</h1>");
+    res.render("hero", {
+        title: "Чико Хикс",
+        picture: "images/chickhicks",
+        desc: "Зеленый Shyster Cremlin 1979",
+    });
 });
 
 /* Страница Салли */
 router.get("/sally", function (req, res, next) {
-    res.send("<h1>Страница Салли</h1>");
+    res.render("hero", {
+        title: "Салли",
+        picture: "images/sally",
+        desc: "Голубой Porsche 911 Carrera 2002",
+    });
 });
 
 module.exports = router;
