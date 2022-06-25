ab7be3188b03982a512bd4efc5818ba654a949c7
Tyman3413 <tyman3413@gmail.com>
2022-06-25 15:11:14 +0300

    point_6.1

---
+++ all/data.js
@@ -0,0 +1,34 @@
+var data = [
+    {
+        title: "Молния МакКуин",
+        nick: "mcqueen",
+        avatar: "images/mcqueen.jpg",
+        desc: "Красный Roockie Race Car 2006",
+    },
+    {
+        title: "Док Хадсон",
+        nick: "dochudson",
+        avatar: "images/dochudson.jpg",
+        desc: "Темно-синий Hudson Hornet 1951",
+    },
+    {
+        title: "Ченг Кинг",
+        nick: "king",
+        avatar: "images/king.jpg",
+        desc: "Голубой Plymouth Superbird 1970",
+    },
+    {
+        title: "Чико Хикс",
+        nick: "chickhicks",
+        avatar: "images/chickhicks.jpg",
+        desc: "Зеленый Shyster Cremlin 1979",
+    },
+    {
+        title: "Салли",
+        nick: "sally",
+        avatar: "images/sally.jpg",
+        desc: "Голубой Porsche 911 Carrera 2002",
+    },
+];
+
+module.exports.data = data;
