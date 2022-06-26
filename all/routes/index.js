28bca167b421031eda23f322f8cbcdd512a67c21
Tyman3413 <tyman3413@gmail.com>
2022-06-26 13:18:43 +0300

    point_12.1

--- all/routes/index.js
+++ all/routes/index.js
@@ -5,13 +5,18 @@ var Hero = require("../models/hero").Hero;
 /* GET home page. */
 router.get("/", function (req, res, next) {
     Hero.find({}, { _id: 0, title: 1, nick: 1 }, function (err, menu) {
-        req.session.greeting = "Hello!";
+        req.session.greeting = "Hi!!!!";
         res.render("index", {
             title: "The Cars",
             menu: menu,
             counter: req.session.counter,
         });
     });
+});
+
+/* GET login/registration page. */
+router.get("/logreg", function (req, res, next) {
+    res.render("logreg", { title: "Вход" });
 });
 
 module.exports = router;
